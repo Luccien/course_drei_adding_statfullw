6eccb7735479960b05c61c94ef3ad00ab93f830d
lucie <lu@>
2023-09-09 08:23:28 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/yourdomain/course_drei_adding_statfullw/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yourdomain.course_drei_adding_statfullw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:course_drei_adding_statfullw/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+}
